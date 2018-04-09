2b7c53119020a067c41385bd488310083a3934c9
Eduardo Antuña <eduadiez@gmail.com>
2018-04-09 18:27:26 +0200

    DAppNode compatibility modifications

--- app/scripts/nodes.js
+++ app/scripts/nodes.js
@@ -34,7 +34,7 @@ nodes.customNodeObj = {
     'lib': null
 };
 nodes.nodeList = {
-    'eth_mycrypto': {
+    'eth_dappnode': {
         'name': 'ETH',
         'blockExplorerTX': 'https://etherscan.io/tx/[[txHash]]',
         'blockExplorerAddr': 'https://etherscan.io/address/[[address]]',
@@ -43,56 +43,8 @@ nodes.nodeList = {
         'chainId': 1,
         'tokenList': require('./tokens/ethTokens.json'),
         'abiList': require('./abiDefinitions/ethAbi.json'),
-        'service': 'mycryptoapi.com',
-        'lib': new nodes.customNode('https://api.mycryptoapi.com/eth', '')
-    },
-    'eth_ethscan': {
-        'name': 'ETH',
-        'blockExplorerTX': 'https://etherscan.io/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://etherscan.io/address/[[address]]',
-        'type': nodes.nodeTypes.ETH,
-        'eip155': true,
-        'chainId': 1,
-        'tokenList': require('./tokens/ethTokens.json'),
-        'abiList': require('./abiDefinitions/ethAbi.json'),
-        'service': 'etherscan.io',
-        'lib': require('./nodeHelpers/etherscan')
-    },
-    'eth_infura': {
-        'name': 'ETH',
-        'blockExplorerTX': 'https://etherscan.io/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://etherscan.io/address/[[address]]',
-        'type': nodes.nodeTypes.ETH,
-        'eip155': true,
-        'chainId': 1,
-        'tokenList': require('./tokens/ethTokens.json'),
-        'abiList': require('./abiDefinitions/ethAbi.json'),
-        'service': 'infura.io',
-        'lib': new nodes.infuraNode('https://mainnet.infura.io/mycrypto')
-    },
-    'eth_giveth': {
-        'name': 'ETH',
-        'blockExplorerTX': 'https://etherscan.io/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://etherscan.io/address/[[address]]',
-        'type': nodes.nodeTypes.ETH,
-        'eip155': true,
-        'chainId': 1,
-        'tokenList': require('./tokens/ethTokens.json'),
-        'abiList': require('./abiDefinitions/ethAbi.json'),
-        'service': 'giveth.io',
-        'lib': new nodes.customNode('https://mew.giveth.io', '')
-    },
-    'etc_epool': {
-        'name': 'ETC',
-        'blockExplorerTX': 'https://gastracker.io/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://gastracker.io/addr/[[address]]',
-        'type': nodes.nodeTypes.ETC,
-        'eip155': true,
-        'chainId': 61,
-        'tokenList': require('./tokens/etcTokens.json'),
-        'abiList': require('./abiDefinitions/etcAbi.json'),
-        'service': 'epool.io',
-        'lib': new nodes.customNode('https://mewapi.epool.io', '')
+        'service': 'DAppNode',
+        'lib': new nodes.customNode('http://my.ethchain.dnp.dappnode.eth:8545', '')
     },
     'rop_infura': {
         'name': 'Ropsten',
@@ -103,8 +55,8 @@ nodes.nodeList = {
         'chainId': 3,
         'tokenList': require('./tokens/ropstenTokens.json'),
         'abiList': require('./abiDefinitions/ropstenAbi.json'),
-        'service': 'infura.io',
-        'lib': new nodes.infuraNode('https://ropsten.infura.io/mycrypto')
+        'service': 'DAppNode',
+        'lib': new nodes.customNode('http://my.ropsten.dnp.dappnode.eth:8545', '')
     },
     'kov_ethscan': {
         'name': 'Kovan',
@@ -115,20 +67,8 @@ nodes.nodeList = {
         'chainId': 42,
         'tokenList': require('./tokens/kovanTokens.json'),
         'abiList': require('./abiDefinitions/kovanAbi.json'),
-        'service': 'etherscan.io',
-        'lib': require('./nodeHelpers/etherscanKov')
-    },
-    'kov_infura': {
-        'name': 'Kovan',
-        'type': nodes.nodeTypes.Kovan,
-        'blockExplorerTX': 'https://kovan.etherscan.io/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://kovan.etherscan.io/address/[[address]]',
-        'eip155': true,
-        'chainId': 42,
-        'tokenList': require('./tokens/kovanTokens.json'),
-        'abiList': require('./abiDefinitions/kovanAbi.json'),
-        'service': 'infura.io',
-        'lib': new nodes.infuraNode('https://kovan.infura.io/mycrypto')
+        'service': 'DAppNode',
+        'lib': new nodes.customNode('http://my.kovan.dnp.dappnode.eth:8545', '')
     },
     'rin_ethscan': {
         'name': 'Rinkeby',
@@ -139,98 +79,8 @@ nodes.nodeList = {
         'chainId': 4,
         'tokenList': require('./tokens/rinkebyTokens.json'),
         'abiList': require('./abiDefinitions/rinkebyAbi.json'),
-        'service': 'etherscan.io',
-        'lib': require('./nodeHelpers/etherscanRin')
-    },
-    'rin_infura': {
-        'name': 'Rinkeby',
-        'blockExplorerTX': 'https://rinkeby.etherscan.io/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://rinkeby.etherscan.io/address/[[address]]',
-        'type': nodes.nodeTypes.Rinkeby,
-        'eip155': true,
-        'chainId': 4,
-        'tokenList': require('./tokens/rinkebyTokens.json'),
-        'abiList': require('./abiDefinitions/rinkebyAbi.json'),
-        'service': 'infura.io',
-        'lib': new nodes.infuraNode('https://rinkeby.infura.io/mycrypto')
-    },
-    'exp': {
-        'name': 'EXP',
-        'blockExplorerTX': 'http://www.gander.tech/tx/[[txHash]]',
-        'blockExplorerAddr': 'http://www.gander.tech/address/[[address]]',
-        'type': nodes.nodeTypes.EXP,
-        'eip155': true,
-        'chainId': 2,
-        'tokenList': require('./tokens/expTokens.json'),
-        'abiList': require('./abiDefinitions/expAbi.json'),
-        'estimateGas': true,
-        'service': 'expanse.tech',
-        'lib': new nodes.customNode('https://node.expanse.tech/', '')
-    },
-    'ubq': {
-        'name': 'UBQ',
-        'blockExplorerTX': 'https://ubiqscan.io/en/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://ubiqscan.io/en/address/[[address]]',
-        'type': nodes.nodeTypes.UBQ,
-        'eip155': true,
-        'chainId': 8,
-        'tokenList': require('./tokens/ubqTokens.json'),
-        'abiList': require('./abiDefinitions/ubqAbi.json'),
-        'estimateGas': true,
-        'service': 'ubiqscan.io',
-        'lib': new nodes.customNode('https://pyrus2.ubiqscan.io', '')
-    },
-    'poa': {
-        'name': 'POA',
-        'blockExplorerTX': 'https://core-explorer.poa.network/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://core-explorer.poa.network/account/[[address]]',
-        'type': nodes.nodeTypes.POA,
-        'eip155': true,
-        'chainId': 99,
-        'tokenList': require('./tokens/poaTokens.json'),
-        'abiList': require('./abiDefinitions/poaAbi.json'),
-        'estimateGas': true,
-        'service': 'core.poa.network',
-        'lib': new nodes.customNode('https://core.poa.network', '')
-    },
-    'tomo': {
-        'name': 'TOMO',
-        'blockExplorerTX': 'https://explorer.tomocoin.io/#/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://explorer.tomocoin.io/#/address/[[address]]',
-        'type': nodes.nodeTypes.TOMO,
-        'eip155': true,
-        'chainId': 40686,
-        'tokenList': require('./tokens/tomoTokens.json'),
-        'abiList': require('./abiDefinitions/tomoAbi.json'),
-        'estimateGas': true,
-        'service': 'core.tomocoin.io',
-        'lib': new nodes.customNode('https://core.tomocoin.io', '')
-    },
-    'ella': {
-        'name': 'ELLA',
-        'blockExplorerTX': 'https://explorer.ellaism.org/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://explorer.ellaism.org/addr/[[address]]',
-        'type': nodes.nodeTypes.ELLA,
-        'eip155': true,
-        'chainId': 64,
-        'tokenList': require('./tokens/ellaTokens.json'),
-        'abiList': require('./abiDefinitions/ellaAbi.json'),
-        'estimateGas': true,
-        'service': 'ellaism.org',
-        'lib': new nodes.customNode('https://jsonrpc.ellaism.org', '')
-    },
-    'etsc': {
-        'name': 'ETSC',
-        'blockExplorerTX': 'https://explorer.ethereumsocial.kr/#/tx/[[txHash]]',
-        'blockExplorerAddr': 'https://explorer.ethereumsocial.kr/#/address/[[address]]',
-        'type': nodes.nodeTypes.ETSC,
-        'eip155': true,
-        'chainId': 214,
-        'tokenList': require('./tokens/etscTokens.json'),
-        'abiList': require('./abiDefinitions/etscAbi.json'),
-        'estimateGas': true,
-        'service': 'gazua.tv',
-        'lib': new nodes.customNode('https://node.ethereumsocial.kr', '')
+        'service': 'DAppNode',
+        'lib': new nodes.customNode('http://my.rinkeby.dnp.dappnode.eth:8545', '')
     }
 };
 
